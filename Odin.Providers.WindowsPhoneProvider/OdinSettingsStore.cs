aef9a1e561fd95c0cf3c7cc583611f0c99fa2ca1
Richard Astbury <richard.astbury@gmail.com>
2014-06-05 10:23:34 +0100

    add a guard

--- Odin.Providers.WindowsPhoneProvider/OdinSettingsStore.cs
+++ Odin.Providers.WindowsPhoneProvider/OdinSettingsStore.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.Storage;
@@ -11,6 +12,7 @@ namespace Odin.Providers.WindowsPhoneProvider
 
         public OdinSettingsStore(ApplicationDataContainer container)
         {
+            if (null == container) throw new ArgumentNullException("container");
             this.Container = container;
         }
 
