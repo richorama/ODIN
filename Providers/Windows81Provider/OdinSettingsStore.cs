c1ab352c11e1262d32d2570fc38911ecf4970f63
Richard Astbury <richard.astbury@gmail.com>
2014-06-18 21:52:56 +0100

    added win81 settings procicer

---
+++ Providers/Windows81Provider/OdinSettingsStore.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Odin.Providers.Windows81Provider
+{
+    public class OdinSettingsStore : IOdin
+    {
+        public ApplicationDataContainer Container { get; private set; }
+
+        public OdinSettingsStore(ApplicationDataContainer container)
+        {
+            if (null == container) throw new ArgumentNullException("container");
+            this.Container = container;
+        }
+
+        public OdinSettingsStore()
+        {
+            this.Container = Windows.Storage.ApplicationData.Current.LocalSettings;
+        }
+
+        public OdinSettingsStore CreateForLocalSettings()
+        {
+            return new OdinSettingsStore(Windows.Storage.ApplicationData.Current.LocalSettings);
+        }
+
+        public OdinSettingsStore CreateForRoamingSettings()
+        {
+            return new OdinSettingsStore(Windows.Storage.ApplicationData.Current.RoamingSettings);
+        }
+
+
+        public async Task Put(string key, string value)
+        {
+            this.Container.Values[key] = value;
+        }
+
+        public async Task<string> Get(string key)
+        {
+            if (this.Container.Values.ContainsKey(key))
+            {
+                return this.Container.Values[key] as string;
+            }
+            return null;
+        }
+
+        public async Task Delete(string key)
+        {
+            this.Container.Values.Remove(key);
+        }
+
+        public Task<IEnumerable<KeyValue>> Search(string start = null, string end = null)
+        {
+            var results = this.Container.Values.OrderBy(x => x.Key);
+            if (!string.IsNullOrWhiteSpace(start)) results = results.Where(x => string.Compare(x.Key, start) >= 0).OrderBy(x => x.Key);
+            if (!string.IsNullOrWhiteSpace(end)) results = results.Where(x => string.Compare(x.Key, end) <= 0).OrderBy(x => x.Key);
+            return Task.FromResult(results.Select(x => new KeyValue { Key = x.Key, Value = x.Value as string }));
+        }
+    }
+}
---
+++ Tests/Windows81/ProviderTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+using Odin.Providers.Windows81Provider;
+
+namespace Odin.Tests.Windows81
+{
+    [TestClass]
+    public class Windows81ProviderTests
+    {
+        [TestMethod]
+        public async Task Windows81()
+        {
+            var settingsStore = new OdinSettingsStore();
+            await BasicOperations(settingsStore);
+        }
+
+
+        public static async Task BasicOperations(IOdin clive)
+        {
+            await clive.Put("foo", "bar");
+            Assert.AreEqual("bar", await clive.Get("foo"));
+            await clive.Put("foo", "baz");
+            Assert.AreEqual("baz", await clive.Get("foo"));
+            await clive.Delete("foo");
+            Assert.AreEqual(null, await clive.Get("foo"));
+            await clive.Delete("foo");
+
+            var tasks = new List<Task>();
+            for (var i = 0; i < 100; i++)
+            {
+                tasks.Add(clive.Put(i.ToString().PadLeft(4, '0'), i.ToString()));
+            }
+            await Task.WhenAll(tasks);
+            tasks.Clear();
+
+            var items = (await clive.Search()).ToArray();
+            Assert.AreEqual(100, items.Length);
+            Assert.AreEqual("0000", items[0].Key);
+            Assert.AreEqual("0099", items[99].Key);
+
+            items = (await clive.Search(start: "0001", end: "0010")).ToArray();
+            Assert.AreEqual(10, items.Length);
+            Assert.AreEqual("0001", items[0].Key);
+            Assert.AreEqual("0010", items[9].Key);
+
+            for (var i = 0; i < 100; i++)
+            {
+                tasks.Add(clive.Delete(i.ToString()));
+            }
+        }
+
+    }
+}
